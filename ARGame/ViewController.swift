ea4e617cafa44e809db49449e5a73a5e6a6f9b43
MaiAbdElmonem <maiabdelmonem278@gmail.com>
2021-10-25 06:25:04 +0200

    Initial Commit

---
+++ ARGame/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARGame
+//
+//  Created by Mai Abd Elmonem on 25/10/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
